b15c0d0e6c9326634b39a1e977480f9179c17917
Maeve Andrews <git@maeveandrews.com>
2020-07-18 04:22:23 -0400

    Retry database connection

--- app/db.rb
+++ app/db.rb
@@ -2,7 +2,14 @@ require 'logger'
 require 'sequel/core'
 require_relative 'lib/helpers/env'
 
-DB = Sequel.connect(ENV_delete_in_prod('PLUM_DATABASE_URL'))
+begin
+  DB = Sequel.connect(ENV_delete_in_prod('PLUM_DATABASE_URL'))
+rescue Sequel::DatabaseConnectionError => e
+  $stderr.puts e.message
+  sleep 1
+  $stderr.puts "Retrying connection..."
+  retry
+end
 
 DB.loggers << Logger.new($stderr).tap do |logger|
   logger.level = Logger::FATAL unless ENV['RACK_ENV'] == 'production'
